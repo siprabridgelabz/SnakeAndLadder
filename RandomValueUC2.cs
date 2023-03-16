dd994a6fdd6d940557530a7d4c60a530689860ff
sipra <sipramishra085@gmail.com>
2023-03-17 00:11:58 +0530

    uc2 added

---
+++ RandomValueUC2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderProblem
+{
+    public  class RandomValueUC2
+    {
+        public static void FindRandomValueUC2()
+        {
+            Random random = new Random();
+            int diceOutput = random.Next(7);
+            int position = 0;
+            Console.WriteLine("The outcome of number : "+diceOutput);
+            Console.ReadLine();
+
+        }
+    }
+}
