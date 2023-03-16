2dba9dbf2f541e53b58e9c84e54bec1051a30c1c
sipra <sipramishra085@gmail.com>
2023-03-17 00:14:55 +0530

    uc5 added

---
+++ WinningPositionUC5.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderProblem
+{
+    public class WinningPositionUC5
+    {
+        
+      
+        
+            public const int CaseOfNoPlay = 0;
+            public const int CaseOfLadder = 1;
+            public const int CaseOfSnake = 2;
+            public const int MaxPosition = 100;
+
+            public static void CheckWinningPositionUC5()
+            {
+                Console.WriteLine("Start the snake and ladder game");
+                Console.WriteLine("Single player at starit position 0");
+                Console.WriteLine(" ");
+                int position = 0;
+                while (position != MaxPosition)
+                {
+                    Random random = new Random();
+                    int RollDice = random.Next(1, 6);
+                    Console.WriteLine("Player rolls the die and get: " + RollDice);
+                    Random random1 = new Random();
+                    int option = random1.Next(3);
+
+                    switch (option)
+                    {
+                        case CaseOfNoPlay:
+                            {
+                                Console.WriteLine("Option  NO PLAY selected " + position);
+                                Console.WriteLine("Player stey at same postion");
+                                break;
+                            }
+
+                        case CaseOfLadder:
+                            {
+                                Console.WriteLine("Option LADDER selected " + position);
+                                Console.WriteLine("Player move ahead " + RollDice);
+                                position = RollDice + position;
+                                break;
+                            }
+
+                        case CaseOfSnake:
+                            {
+                                Console.WriteLine("Option SNAKE is selected " + (RollDice + position));
+                                Console.WriteLine("Player move behind by " + RollDice);
+                                position = position - RollDice;
+                                break;
+                            }
+                    }
+                    if (position < 0)
+                    {
+                        position = 0;
+                        Console.WriteLine("Player have to restart from position " + position);
+                    }
+                    if (position > MaxPosition)
+                    {
+                        position = position - RollDice;
+                        Console.WriteLine(" Your chance is skip. Stay at same position " + position);
+                    }
+                    Console.WriteLine("Current position of player is " + position);
+                    Console.WriteLine(" ");
+                }
+            }
+        
+
+    }
+}
