8c50bd348a35649d419a4ed428a5cab41cfeb9dc
sipra <sipramishra085@gmail.com>
2023-03-17 00:06:58 +0530

    file added

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SnakeLadderProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //  Console.WriteLine("Welcome in Snake and Ladder Problem");
+            //StartUC1.FindStartUC1();
+            // RandomValueUC2.FindRandomValueUC2();
+             //CheckOptionUC3.CasePosition();
+            // PlayerPositionUC4.FindPlayerPositionUC4();
+            // WinningPositionUC5.CheckWinningPositionUC5();
+            // DiceRollUC6.NumberOfDice();
+            GameWith2PlayerUC7.SLWithTwoPlayer();
+        }
+    }
+}
